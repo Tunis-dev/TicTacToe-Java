6320f75aceda3ebbe3fc8211645c9dbb806332f8
BIT504 <tuniscook@gmail.com>
2021-05-07 00:16:07 +1200

    first commit and push to github for tictactoe

---
+++ BIT504_Assignment_2_PartA_Student5019194_BIT504_Assign2_PartAStudent/DrawCanvas.java
@@ -0,0 +1,15 @@
+import java.awt.Dimension;
+
+public class DrawCanvas {
+
+	public void mouseClicked() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPreferredSize(Dimension dimension) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
